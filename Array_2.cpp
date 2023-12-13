0c243ec8dd1f38773bec6064f1a0f050fbbf21fa
Ваня Витюк <naharamimo@gmail.com>
2023-12-13 21:13:04 +0200

    Добавьте файлы проекта.

---
+++ Array_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+int countNumbersWithEvenDigits(const std::vector<int>& nums) {
+    int count = 0;
+
+    for (int num : nums) {
+        int digitCount = 0;
+        int temp = num;
+        while (temp != 0) {
+            temp /= 10;
+            digitCount++;
+        }
+
+        if (digitCount % 2 == 0) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    std::vector<int> nums1 = { 12, 345, 2, 6, 7896 };
+    std::cout << "Example 1 Output: " << countNumbersWithEvenDigits(nums1) << std::endl;
+
+    std::vector<int> nums2 = { 555, 901, 482, 1771 };
+    std::cout << "Example 2 Output: " << countNumbersWithEvenDigits(nums2) << std::endl;
+
+    return 0;
+}
